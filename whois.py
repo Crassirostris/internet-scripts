593a13f17b4eefdab3b6cead7bf77a677fac6942
Crassirostris <crassirostris@yandex.com>
2015-02-07 16:15:59 +0500

    whois initial version

---
+++ whois.py
@@ -0,0 +1,37 @@
+import argparse
+from ipaddress import IPv4Address
+from select import select
+from socket import socket, AF_INET, SOCK_STREAM
+
+DEFAULT_WHOIS_PORT = 43
+DEFAULT_WOIS_PROVIDER = "whois.ripe.net"
+
+BUFFER_SIZE = 4 * 1024
+
+
+def get_address(address_string):
+    chunks = address_string.split(':')
+    return chunks[0], int(chunks[1]) if len(chunks) > 1 else DEFAULT_WHOIS_PORT
+
+
+def recv_all(sock):
+    result = b''
+    data = b'123'
+    while select([sock], [], [], 0.25)[0] and len(data) > 0:
+        data = sock.recv(BUFFER_SIZE)
+        result += data
+    return result
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Whois tool")
+    parser.add_argument("address", help="IP address to resolve")
+    parser.add_argument("source", nargs="?", default="%s:%d" % (DEFAULT_WOIS_PROVIDER, DEFAULT_WHOIS_PORT), help="Source server address")
+    args = parser.parse_args()
+    address = get_address(args.source)
+    with socket(AF_INET, SOCK_STREAM) as sock:
+        sock.connect(address)
+        sock.setblocking(0)
+        print(recv_all(sock).decode('utf-8'))
+        sock.sendall((str(IPv4Address(args.address)) + "\r\n").encode('utf-8'))
+        print(recv_all(sock).decode('utf-8'))
