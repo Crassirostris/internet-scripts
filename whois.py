de51e6d225d4a68e7ee71748d44d70f148fc1aa5
Crassirostris <crassirostris@yandex.com>
2015-02-16 20:58:26 +0500

    Added exception handling, did some refactoring

--- whois.py
+++ whois.py
@@ -3,22 +3,24 @@
 import argparse
 from ipaddress import IPv4Address
 from select import select
-from socket import socket, AF_INET, SOCK_STREAM
+import socket
 
 DEFAULT_WHOIS_PORT = 43
 DEFAULT_WHOIS_PROVIDER = "whois.ripe.net"
+SOCKET_CONNECT_TIMEOUT = 1
+SOCKET_POLLING_PERIOD = 0.25
 
 BUFFER_SIZE = 4 * 1024
 
 
-def get_address(address_string):
+def get_socket_address(address_string):
     chunks = address_string.split(':')
     return chunks[0], int(chunks[1]) if len(chunks) > 1 else DEFAULT_WHOIS_PORT
 
 
 def recv_all(sock):
     result = b''
-    while select([sock], [], [], 0.25)[0]:
+    while select([sock], [], [], SOCKET_POLLING_PERIOD)[0]:
         data = sock.recv(BUFFER_SIZE)
         if len(data) == 0:
             break
@@ -26,15 +28,29 @@ def recv_all(sock):
     return result
 
 
+def get_local_machine_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+
+def receive_information(target):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(SOCKET_CONNECT_TIMEOUT)
+        sock.connect(socket_address)
+        sock.setblocking(0)
+        result = recv_all(sock).decode('utf-8')
+        sock.sendall((target + "\r\n").encode('utf-8'))
+        result += recv_all(sock).decode('utf-8')
+    return result
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Whois tool")
-    parser.add_argument("address", help="IP address to resolve")
+    parser.add_argument("target", nargs="?", default=get_local_machine_ip(), help="IP address to resolve")
     parser.add_argument("source", nargs="?", default="%s:%d" % (DEFAULT_WHOIS_PROVIDER, DEFAULT_WHOIS_PORT), help="Source server address")
     args = parser.parse_args()
-    address = get_address(args.source)
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        sock.connect(address)
-        sock.setblocking(0)
-        print(recv_all(sock).decode('utf-8'))
-        sock.sendall((str(IPv4Address(args.address)) + "\r\n").encode('utf-8'))
-        print(recv_all(sock).decode('utf-8'))
+    socket_address = get_socket_address(args.source)
+    try:
+        target = str(IPv4Address(args.target))
+        print(receive_information(target))
+    except Exception:
+        print("Failed to request info about '%s' from '%s'" % (args.target, args.source))
